9371bd07b72342f21d1aa4fdff8dc095901764ea
konradkarwowski <konrad.bipc@gmail.com>
2022-12-27 15:17:08 -0800

    Add ability to enter more than one digit at a time into the display.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Final project assignment of the Fundamentals portion from
  The Odin project coding bootcamp
 
+Still need/for review:
+- account for someone entering a number that is more than one digit
+- do i need the id's on the number buttons? 
--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
+let displayValue = 0;
+
+
 const btns = document.querySelectorAll('.numberBtn');
 console.log(btns);
 btns.forEach(btn => {
     btn.addEventListener('click', () => { 
-    document.querySelector('.display').textContent = btn.textContent;
+    if (displayValue == 0) {
+      displayValue = btn.textContent;}
+    else {
+      displayValue += btn.textContent;}
+    document.querySelector('.display').textContent = displayValue;
   });
 });
 
