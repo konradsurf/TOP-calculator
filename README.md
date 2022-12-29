12286d6f131fc1bec434c4f06bb26b6946fc2414
konradkarwowski <konrad.bipc@gmail.com>
2022-12-29 12:19:31 -0800

    Add clear all function

--- README.md
+++ README.md
@@ -3,5 +3,14 @@ Final project assignment of the Fundamentals portion from
  The Odin project coding bootcamp
 
 Still need/for review:
-- account for someone entering a number that is more than one digit
+- account for someone entering a number that is more than one digit - DONE
+
 - do i need the id's on the number buttons? 
+
+- should the decimal (.) be added to numberBtn class?
+  - if so, how do i prevent zero from dissapering when event lsitern function
+    runs?  perhaps remove but have conditional that if it's a decimal, 
+    then add the zero back?
+
+- function and event listener for clear all button
+
--- javascript.js
+++ javascript.js
@@ -1,15 +1,78 @@
 let displayValue = 0;
+let subTotal;
+let firstEntry = true;
+enterNumbers();
+enterOperator();
+clearAll();
 
 
-const btns = document.querySelectorAll('.numberBtn');
-console.log(btns);
-btns.forEach(btn => {
+
+
+// FUNCTIONS
+function operate(a, b, operator) {
+  switch(operator) {
+    case 'divide':
+      document.querySelector('.display').textContent = divide(a, b);
+      break;
+    case 'multiply':
+      document.querySelector('.display').textContent = multiply(a, b);
+      break;
+    case 'add':
+      document.querySelector('.display').textContent = add(a, b);
+      break;
+    case 'subtract':
+      document.querySelector('.display').textContent = subtract(a, b);
+      break;
+  };
+  return;
+}
+
+function enterNumbers() {
+  const btns = document.querySelectorAll('.numberBtn');
+  btns.forEach(btn => {
     btn.addEventListener('click', () => { 
     if (displayValue == 0) {
       displayValue = btn.textContent;}
     else {
       displayValue += btn.textContent;}
     document.querySelector('.display').textContent = displayValue;
+    });
   });
-});
+}
 
+function enterOperator() {
+  const opBtns = document.querySelectorAll('.operatorBtn');
+    opBtns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        let x = displayValue, operator = btn.id;
+        // PLACEHOLDER FOR Y BELOW FOR TESTING
+        let y = 2;
+        operate(x, y, operator);
+      });  
+    });
+  }
+
+function clearAll() {
+  const clearBtn = document.querySelector('#clear');
+  clearBtn.addEventListener('click', () => {
+    displayValue = 0;
+    document.querySelector('.display').textContent = displayValue;
+    subTotal = 0;
+  });
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b; 
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
