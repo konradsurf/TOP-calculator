0785d63994fd630a904acd412fb9d5b389e79084
konradkarwowski <konrad.bipc@gmail.com>
2023-01-03 15:36:10 -0800

    Add user entry of first operator. First number in operation will still be pre-defined at initialization

--- javascript.js
+++ javascript.js
@@ -1,58 +1,62 @@
 let firstOperatorEntry = true;
 let enteredNumber = null;
-let subTotal = 43;
-let enteredOperator = 'multiply';
-let priorOperator = 'add';
-let display = document.querySelector('.display');
+let subTotal = null;
+let enteredOperator = "multiply";
+let priorOperator = null;
+let display = document.querySelector(".display");
 
-const opBtns = document.querySelectorAll('.operatorBtn');
-opBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    subTotal = operate(btn.id, subTotal, enteredNumber);
-    document.querySelector('.display').textContent = subTotal;  
-  })
-});  
+/* add user entry of first operator - first number in operation
+will still be pre-defined at initialization */
+const opBtns = document.querySelectorAll(".operatorBtn");
+opBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // may be able to replace firstOperatorEntry with test for subTotal == null
+    if (firstOperatorEntry == true) {  
+      priorOperator = btn.id;
+      subTotal = enteredNumber;
+      enteredNumber = null;
+      firstOperatorEntry = false;
+    } else {
+      subTotal = operate(priorOperator, subTotal, enteredNumber);
+      document.querySelector(".display").textContent = subTotal;
+      // need to add some more here so next operation in chain works
+    }
+  });
+});
 
-
-const numBtns = document.querySelectorAll('.numberBtn');
-numBtns.forEach(btn => {
-  btn.addEventListener('click', () => { 
+const numBtns = document.querySelectorAll(".numberBtn");
+numBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // if nothing in entered number then assign else concatenate
     if (enteredNumber == null) {
       enteredNumber = btn.textContent;
       display.textContent = enteredNumber;
-    }
-    else {
+    } else {
       enteredNumber += btn.textContent;
       display.textContent = enteredNumber;
     }
-  })
+  });
 });
 
-
-
-
-
 function operate(operator, a, b) {
-  switch(operator) {
-    case 'divide':
+  switch (operator) {
+    case "divide":
       return divide(a, b);
-    case 'multiply':
-      return multiply(a,b);
-    case 'add':
+    case "multiply":
+      return multiply(a, b);
+    case "add":
       return add(a, b);
-    case 'subtract':
+    case "subtract":
       return subtract(a, b);
-  };
+  }
 }
-
-
 
 function add(a, b) {
   return a + b;
 }
 
 function subtract(a, b) {
-  return a - b; 
+  return a - b;
 }
 
 function divide(a, b) {
